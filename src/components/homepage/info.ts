0f4ddb43d5232bc7769e62f603d3a430e8c76cd6
Stephanie.Gao <tm1997@163.com>
2021-09-12 22:17:00 +0800

    feat: optimization

---
+++ src/components/homepage/info.ts
@@ -0,0 +1,21 @@
+import { IHomepageModule } from './interface';
+
+export const info: IHomepageModule[] = [{
+  title: 'Intro.',
+  content: [
+    'I got my bachelor\'s degree in Computer Science from <a class="common-content-link" href="https://www.seu.edu.cn/english/" target="_blank" rel="noopener noreferrer">Southeast University</a> in 2019. After that I started working full-time as a frontend developer, mainly using React.',
+    'The reason I chose to be a frontend developer is that I found myself interested in User Interface and I could empathize with users of my application.',
+    'Recently, Computer Graphics has become my new learning direction and I\'m working on WebGL these days. You can find my personal projects <a class="common-content-link" href="/projects">here</a>.',
+  ],
+}, {
+  title: 'Life.',
+  content: [
+    'Music plays an important part in my life. I was obsessed with fingerstyle in 2015, since then I started to teach myself to play guitar. I listen to Rock, Indie and Cantopop and I\'m also a big fan of YouTubers like Walk off the Earth.',
+    'Besides, I\'m also a shutterbug. You can check out my posts on <a class="common-content-link" href="https://unsplash.com/@stephanie_tm" target="_blank" rel="noopener noreferrer">Unsplash</a>.',
+  ],
+}, {
+  title: 'Contact.',
+  content: [
+    'You can contact me by Email <a class="common-content-link" href="mailto:stephanietm1997@gmail.com">stephanietm1997@gmail.com</a> and find me at <a class="common-content-link" href="https://github.com/StephanieTM" target="_blank" rel="noopener noreferrer">GitHub</a>.',
+  ],
+}];
