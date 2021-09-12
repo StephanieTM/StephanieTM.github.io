8f2ae2dd3017a697725302bdfd492b1df2a04c5a
Stephanie.Gao <tm1997@163.com>
2021-09-12 21:02:55 +0800

    feat: update homepage content

--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -6,8 +6,10 @@ export default function HomePage(): JSX.Element {
   return (
     <div className="homepage">
       <p>Hello, I&apos;m Stephanie Gao from China.</p>
-      <p>I work full-time as a frontend developer, mainly using React. Lately I found myself interested in Computer Graphics and I&apos;m working on WebGL these days. You can find my personal projects <Link className="common-content-link" to="/projects">here</Link>.</p>
-      <p>Music plays an important part in my life. I love fingerstyle so I started teaching myself to play guitar since 2015. I listen to Rock, Indie and Cantopop.</p>
+      <p>After getting my bachelor&apos;s degree in Computer Science from <a className="common-content-link" href="https://www.seu.edu.cn/english/" target="_blank" rel="noopener noreferrer">Southeast University</a> in 2019, I started working full-time as a frontend developer, mainly using React.</p>
+      <p>The reason I chose to be a frontend developer is that I found myself interested in User Interface and I could empathize with users of my application.</p>
+      <p>Recently, Computer Graphics has become my new learning direction and I&apos;m working on WebGL these days. You can find my personal projects <Link className="common-content-link" to="/projects">here</Link>.</p>
+      <p>Music plays an important part in my life. I was obsessed with fingerstyle in 2015, since then I started to teach myself to play guitar. I listen to Rock, Indie and Cantopop and I&apos;m also a big fan of YouTubers like Walk off the Earth.</p>
       <p>Besides, I&apos;m also a shutterbug. You can check out my posts on <a className="common-content-link" href="https://unsplash.com/@stephanie_tm" target="_blank" rel="noopener noreferrer">Unsplash</a>.</p>
       <p>Contact me by Email <a className="common-content-link" href="mailto:stephanietm1997@gmail.com">stephanietm1997@gmail.com</a> and find me at <a className="common-content-link" href="https://github.com/StephanieTM" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
     </div>
